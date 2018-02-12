ab548e8d999cf8ba1c9e289ba02ae82cc67ec220
drichard <david@drichard.org>
2018-02-11 17:06:19 -0800

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import classNames from 'classnames'
+import { start, stop } from './speech';
+
+import './bootstrap.min.css';
+import './App.css';
+
+const Player = ({ num, isActive, onSelect }) => (
+  <label className={classNames('btn', 'btn-primary', 'player', { active: isActive })} onClick={() => onSelect(num)}>
+    <input type="radio" name="numPlayers" id={num} />
+    {num}
+  </label>
+);
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPlayers: 7,
+      isSpeaking: false,
+      startingPlayer: null
+    };
+  }
+
+  handleStart = () => {
+    const { isSpeaking, numPlayers } = this.state;
+
+    if (isSpeaking) {
+      this.setState({ isSpeaking: false });
+      stop();
+    } else {
+      this.setState({
+        isSpeaking: true,
+        startingPlayer: Math.floor(Math.random() * numPlayers) + 1
+      });
+      start(numPlayers);
+    }
+  }
+
+  handleSelectPlayer = (num) => {
+    this.setState({ numPlayers: num });
+  }
+
+  render() {
+    const { isSpeaking, numPlayers, startingPlayer } = this.state;
+
+    return (
+      <div className="container">
+        <h1 className="py-4 text-center">AVALON COMPANION</h1>
+
+        <div className="py-4">
+          <h4>Players</h4>
+          <div className="btn-group btn-group-toggle d-flex">
+            {[7, 8, 9, 10].map((num) => (
+              <Player key={num} num={num} isActive={num === numPlayers} onSelect={this.handleSelectPlayer} />
+            ))}
+          </div>
+        </div>
+
+        <div className="py-4">
+          <h4>Characters</h4>
+          Coming soon...
+        </div>
+
+        <div className="_fixed-bottom py-4">
+          <button onClick={this.handleStart} type="button" className="btn btn-primary btn-lg btn-block">
+            {isSpeaking ? "Stop" : "Start"}
+          </button>
+        </div>
+        {startingPlayer && <h2 className="py-2 text-center">Starting player: {startingPlayer}</h2>}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/speech.js
@@ -0,0 +1,131 @@
+const synth = window.speechSynthesis;
+
+let voice = null;
+function initVoices() {
+  // iOS
+  voice = synth.getVoices().find((voice) => voice.name === 'Arthur');
+
+  if (!voice) {
+    // Chrome / OSX
+    voice = synth.getVoices().find((voice) => voice.name === 'Daniel');
+  }
+  
+  if (!voice) {
+    // default to first
+    voice = synth.getVoices()[0];
+  }
+}
+
+let currentMsg;
+const speak = (text) => {
+  const msg = new SpeechSynthesisUtterance(text);
+  currentMsg = msg;
+  msg.voice = voice;
+  msg.pitch = 0.9;
+  msg.rate = 0.9;
+  synth.speak(msg);
+  
+  return msg;
+}
+
+function pause(seconds) {
+  currentMsg.onend = function () {
+    // iOS hack: pause on next tick, otherwise it doesn't pause
+    setTimeout(() => {
+      synth.pause();
+  
+      setTimeout(() => {
+        synth.resume();
+      }, seconds * 1000);
+    });
+  }
+}
+
+const pick = (strings) => {
+  const i = Math.floor(Math.random() * strings.length);
+  return strings[i];
+}
+
+const intro = () => pick([
+  `Everyone, close your eyes and put your fists on the table!`,
+  `Everyone, be so kind to close your eyes and put your fist upon the table.`
+]);
+
+const badGuysRevealEachOther = (numPlayers) => {
+  let characters;
+  let pairsOfEyes;
+
+  if (numPlayers === 10) {
+    characters = 'Darth Sidius, Asaj the Assassin, and Jango Fat';
+    pairsOfEyes = 'two pairs';
+  } else {
+    characters = 'Darth Sidius and Asaj the Assassin'
+    pairsOfEyes = 'one pair'
+  }
+
+  return `${characters}, open your eyes and look around to see the agents of evil.
+You should see ${pairsOfEyes} of eyes. If not, then the game is broken, please let the group know.`;
+};
+
+
+const badGuysRevealToYoda = (numPlayers) => {
+  let characters;
+  let numHands;
+
+  if (numPlayers === 10) {
+    characters = 'Asaj the Assassin, Jango Fat and Bad Anakin';
+    numHands = 'three';
+  } else {
+    characters = 'Asaj the Assassin and Bad Anakin'
+    numHands = 'two'
+  }
+
+  return `${characters} open your hands and wiggle your fingers.
+    Yoda, open your eyes and see ${numHands} open hands. These are all the bad guys except for Darth Sidius who you cannot see.`;
+};
+
+const badAnakinReveal = () => `Bad Anakin open your hand and wiggle your fingers so that the bad guys can see you.`;
+
+const closeEverything = () => pick([
+  `Everyone, close your eyes and hands.`,
+  `Everyone, close everything.`
+]);
+
+const anakinsReveal = () => `Bad Anakin and good Anakin open your hands. Padmé open your eyes and see the two Anakins.`
+
+const obiwanReveal = () => `Darth Sidius and Yoda open your hands. Obi Wan, open your eyes and see two open hands.
+One of them is Yoda, the other one is Darth Sidius.`;
+
+const openEyes = () => `Everyone, please open your eyes.`;
+
+export function start(numPlayers) {
+  initVoices();
+
+  speak(intro());
+  pause(2);
+
+  speak(badGuysRevealEachOther(numPlayers));
+  pause(4);
+  speak(badAnakinReveal());
+  pause(3);
+  speak(closeEverything());
+
+  speak(badGuysRevealToYoda(numPlayers));
+  pause(3);
+  speak(closeEverything());
+
+  speak(anakinsReveal());
+  pause(3);
+  speak(closeEverything());
+
+  speak(obiwanReveal());
+  pause(3);
+  speak(closeEverything());
+  pause(2);
+  speak(openEyes());
+}
+
+export function stop() {
+  synth.pause();
+  synth.cancel();
+}
